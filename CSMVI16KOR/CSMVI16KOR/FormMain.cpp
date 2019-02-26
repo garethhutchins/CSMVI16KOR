e4b3f8043fb258a4cbdf6c942ed7ffb2b5583d28
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-02-26 09:04:02 +0000

    Initial Push

---
+++ CSMVI16KOR/CSMVI16KOR/FormMain.cpp
@@ -0,0 +1,14 @@
+#include "FormMain.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	CSMVI16KOR::FormMain form;
+	Application::Run(%form);
+	return 0;
+}
+
