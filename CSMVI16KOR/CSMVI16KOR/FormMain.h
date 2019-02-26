0706e7c849aa4d32501f5642f1d47394d4db3d81
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-02-26 09:14:25 +0000

    Added Windows Form

--- CSMVI16KOR/CSMVI16KOR/FormMain.h
+++ CSMVI16KOR/CSMVI16KOR/FormMain.h
@@ -48,11 +48,17 @@ namespace CSMVI16KOR {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"FormMain";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// FormMain
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(555, 350);
+			this->Name = L"FormMain";
+			this->Text = L"FormMain";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
