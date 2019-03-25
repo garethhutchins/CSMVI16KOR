dd74c9ccfbfbd7b3a6c0ab6de0ead53e9ea8f402
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-03-25 19:48:27 +0100

    Added Folder create

--- CSMVI16KOR/CSMVI16KOR/LabelFolder.cpp
+++ CSMVI16KOR/CSMVI16KOR/LabelFolder.cpp
@@ -1,6 +1,12 @@
 #include "LabelFolder.h"
 #include <string>
+#include "windows.h"
+
 
 LabelFolder::LabelFolder(std::string path, std::string label, int sFrame,int eFrame){
+	std::string t = path + "/" + label;
+	std::wstring stemp = std::wstring(t.begin(), t.end());
+	LPCWSTR sw = stemp.c_str();
+	CreateDirectory(sw, NULL);
 
 	}
